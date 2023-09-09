3e33ee46157818fd10aa063020eb1587a9be5742
Nemanja Grujic <nemanjag3005@gmail.com>
2023-09-08 21:50:30 -0600

    init repo

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full">
+      <div className=" max-w-7xl"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
